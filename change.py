5c046d5099c6ae78e8892e78c55640d318a54c17
Ezequiel Tillerias Ley Matias Bastian <matias.tillerias@conexlan.cl>
2022-10-01 01:57:38 -0300

    Get file Paths fron conf file

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
+import sqlite3
+import re
 
+def get_file_path():
+    a = open("/etc/change-detector/change-detector.conf", "r+")
+    files = []
+    for i in a:
+        if i == "" or i == "\n":
+            continue
+        try:
+            filepath = re.search("(?<=FilePath ).*",i, re.MULTILINE)
+            files.append(filepath[0])
+        except:
+            pass
+    print(files)
 def main():
-    pass    
+        get_file_path()    
